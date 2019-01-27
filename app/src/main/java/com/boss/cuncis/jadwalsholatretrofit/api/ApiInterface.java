00ee5e70540be7506751f697168ad7c3e7098172
cuncis1st <cuncis1st@gmail.com>
2019-01-27 10:26:12 +0700

    Second Commit

---
+++ app/src/main/java/com/boss/cuncis/jadwalsholatretrofit/api/ApiInterface.java
@@ -0,0 +1,15 @@
+package com.boss.cuncis.jadwalsholatretrofit.api;
+
+import com.boss.cuncis.jadwalsholatretrofit.model.Item;
+import com.boss.cuncis.jadwalsholatretrofit.model.ItemResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ApiInterface {
+    @GET("surabaya/{periode}/dayli.json")
+    Call<ItemResponse> getJadwalSholat(@Path("periode") String periode);
+}
